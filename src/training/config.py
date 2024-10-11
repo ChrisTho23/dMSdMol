be50dbed141df8770a6594af7c5823d0d08498a6
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-10-11 18:44:41 +0200

    Adapt training config class to include all necessary parameters

--- src/training/config.py
+++ src/training/config.py
@@ -1,22 +1,27 @@
 from dataclasses import dataclass
+from typing import Any, Dict
+
 
 @dataclass
 class SageMakerTrainingConfig:
+    instance_type: str
+    instance_count: int
     repo: str
     branch: str
     dataparallel: bool
-    instance_type: str
-    instance_count: int
     model_name: str
-    per_device_train_batch_size: int = 4
-    per_device_eval_batch_size: int = 4
-    model_name_or_path: str = 'facebook/bart-large-cnn'
-    dataset_name: str = 'samsum'
-    do_train: bool = True
-    do_predict: bool = True
-    predict_with_generate: bool = True
-    output_dir: str = '/opt/ml/model'
-    num_train_epochs: int = 3
-    learning_rate: float = 5e-5
-    seed: int = 42
-    fp16: bool = True
+    hyperparameters: Dict[str, Any] = {
+        'model_name_or_path': '', # TODO: Add model path once uploaded
+        'tokenizer_name': '', # TODO: Add tokenizer path once uploaded
+        'dataset_name': '', # TODO: Add dataset name once uploaded
+        'output_dir': '/opt/ml/model',
+        'per_device_train_batch_size': 4,
+        'per_device_eval_batch_size': 4,
+        'do_train': True,
+        'do_predict': True,
+        'predict_with_generate': True,
+        'num_train_epochs': 3,
+        'learning_rate': 5e-5,
+        'seed': 42,
+        'fp16': True,
+    }
