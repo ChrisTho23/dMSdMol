b48c31b0ccb932aff6c5dc7c88a55992bcd2a0e8
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-10-13 09:55:45 +0200

    Add config for training on Sagemaker

--- src/training/config.py
+++ src/training/config.py
@@ -1,6 +1,7 @@
 """Configuration for distributed training on SageMaker."""
 
 from dataclasses import dataclass
+from typing import Dict
 
 
 @dataclass
@@ -15,3 +16,17 @@ class Mol2MSTrainingConfig:
     max_length: int = 128
     seed: int = 42
     fp16: bool = False
+
+@dataclass
+class SageMakerTrainingConfig:
+    output_dir: str = "/opt/ml/output"
+    entry_point: str = "train_script.py"
+    source_dir: str = "./src/training"
+    instance_type: str = "ml.p3.2xlarge"
+    instance_count: int = 1
+    transformers_version: str = '4.45'
+    pytorch_version: str = '2.4'
+    py_version: str = 'py310'
+    git_config: Dict[str, str] = {"repo": "https://github.com/ChrisTho23/dMSdMol", "branch": "sagemaker-training"}
+    distribution: Dict[str, Dict[str, bool]] = {"smdistributed": {"dataparallel": {"enabled": True}}}
+
