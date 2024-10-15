92d270c59d130fa1b36bcfa5d6164c43f6f006d4
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-10-15 11:12:08 +0200

    Adapt sagemaker training config for training on sagemaker

--- src/training/config.py
+++ src/training/config.py
@@ -47,7 +47,7 @@ class SageMakerTrainingConfig:
         default="./", metadata={"help": "Source directory containing the training code"}
     )
     instance_type: str = field(
-        default="ml.p3.16xlarge", metadata={"help": "EC2 instance type for training"}
+        default="ml.p4d.24xlarge", metadata={"help": "EC2 instance type for training"}
     )
     instance_count: int = field(
         default=1, metadata={"help": "Number of EC2 instances to use for training"}
@@ -60,7 +60,7 @@ class SageMakerTrainingConfig:
         default="4.28.1", metadata={"help": "Version of Transformers library to use"}
     )
     pytorch_version: str = field(
-        default="2.0.0", metadata={"help": "Version of PyTorch to use"}
+        default="2.2.0", metadata={"help": "Version of PyTorch to use"}
     )
     py_version: str = field(default="py310", metadata={"help": "Python version to use"})
     git_config: Dict[str, str] = field(
@@ -86,6 +86,6 @@ class SageMakerTrainingConfig:
         metadata={"help": "Environment variables for SageMaker training"},
     )
     image_uri: str = field(
-        default="763104351884.dkr.ecr.us-east-1.amazonaws.com/huggingface-pytorch-training:2.0.0-transformers4.28.1-gpu-py310-cu118-ubuntu20.04",
+        default="763104351884.dkr.ecr.us-east-1.amazonaws.com/pytorch-training:2.2.0-gpu-py310-cu121-ubuntu20.04-sagemaker",
         metadata={"help": "Image URI for SageMaker training"},
     )
