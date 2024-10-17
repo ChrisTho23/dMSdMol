7b81b8b7c6595fad592866b84efa86aa21ce77e2
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-10-17 09:07:45 +0200

    Reduce max_ms_length to maximum number of peaks in dataset

--- src/model/config.py
+++ src/model/config.py
@@ -9,6 +9,6 @@ class Mol2MSModelConfig:
     collision_energy_dim: int = 87
     instrument_type_dim: int = 51
     max_length: int = 128
-    max_ms_length: int = 1000
+    max_ms_length: int = 512
     num_heads: int = 16
     num_layers: int = 12
