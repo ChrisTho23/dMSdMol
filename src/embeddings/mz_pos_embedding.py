130206c0144abc7c20964f551c7f76f48921fc17
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-10-18 22:20:04 +0200

    Correct size of freq

--- src/embeddings/mz_pos_embedding.py
+++ src/embeddings/mz_pos_embedding.py
@@ -20,7 +20,7 @@ class MZPositionalEncoding(nn.Module):
         self.normalize = normalize
 
         # Frequency components for encoding
-        freq = freq_scale * torch.exp(-2.0 * torch.arange(d_model // 2) * (math.log(1e4) / d_model))
+        freq = freq_scale * torch.exp(-2.0 * torch.arange(d_model // 2) * (math.log(1e4) / d_model)).repeat(2)
         self.register_buffer('freq', freq)
 
         # Phase shifts for cosine and sine (π/2 shift)
@@ -28,25 +28,22 @@ class MZPositionalEncoding(nn.Module):
         cos_shifts = _sin2cos_phase_shift * (torch.arange(d_model) % 2)
         self.register_buffer('cos_shifts', cos_shifts)
         
-    def forward(self, continuous_values: Float[Tensor, "batch seq"]) -> Float[Tensor, "batch seq d_model"]:
+    def forward(self, tensor: Float[Tensor, "batch seq"]) -> Float[Tensor, "batch seq d_model"]:
         """
         Args:
-            continuous_values (Tensor): Continuous input tensor of shape (batch_size, seq).
+            tensor (Tensor): Continuous input tensor of shape (batch_size, seq).
         
         Returns:
             Tensor: Positional encoding of shape (batch_size, seq, d_model).
         """
-        batch_size, seq = continuous_values.shape
-        
         # Optionally normalize the continuous values (to focus on relational distances)
         if self.normalize:
-            continuous_values = continuous_values - torch.mean(continuous_values, dim=1, keepdim=True)
+            tensor = tensor - torch.mean(tensor, dim=1, keepdim=True)
         
-        # Expand continuous values to shape (batch_size, seq, 1) to prepare for multiplication
-        continuous_values = continuous_values.unsqueeze(-1)
+        tensor = tensor.unsqueeze(-1)
 
         # Compute the product of positions and frequencies for encoding
-        product = continuous_values * self.freq
+        product = tensor * self.freq
         
         # Apply sine and cosine functions to get the positional embeddings
         pos_emb = torch.sin(product + self.cos_shifts)
