15696c7e0bab9f179cf84f8d855c9fbb26555f1c
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-10-18 21:07:55 +0200

    Adapt config

--- src/model/config.py
+++ src/model/config.py
@@ -8,7 +8,8 @@ class Mol2MSModelConfig:
     encoder_name: str = "gayane/BARTSmiles"
     collision_energy_dim: int = 87
     instrument_type_dim: int = 51
-    max_length: int = 128
-    max_ms_length: int = 512
+    dropout: float = 0.1
+    max_encoder_length: int = 128
+    max_decoder_length: int = 512
     num_heads: int = 16
     num_layers: int = 12
