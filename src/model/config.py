4e058c4e96e35fc006bc1936e48df3782899708a
gtcha2 <aron7628@gmail.com>
2024-10-18 04:07:04 -0700

    added initial data classes for the model configs outside of mol2ms

--- src/model/config.py
+++ src/model/config.py
@@ -12,3 +12,20 @@ class Mol2MSModelConfig:
     max_ms_length: int = 512
     num_heads: int = 16
     num_layers: int = 12
+@dataclass
+class MS2MolModelConfig:
+    encoder_name: str = "gayane/BARTSmiles"
+    collision_energy_dim: int = 87
+    instrument_type_dim: int = 51
+    max_length: int = 128
+    max_ms_length: int = 512
+    num_heads: int = 16
+    num_layers: int = 12
+
+#TODO: rename this
+@dataclass
+class DeMy:
+    mol_ms_config: Mol2MSModelConfig=Mol2MSModelConfig()
+    ms_mol_config: MS2MolModelConfig =MS2MolModelConfig()
+    other:dict = (arbitraryDict:={})
+    
