6a465856c3987913daf8b1f0613534c97db80f22
gtcha2 <aron7628@gmail.com>
2024-10-18 04:08:01 -0700

    started more changes to collate functions

--- src/training/collate_functions.py
+++ src/training/collate_functions.py
@@ -3,25 +3,40 @@ from torch.nn.utils.rnn import pad_sequence
 import torch
 
 
-def collate_fn_msz(batch):
-    input_ids = pad_sequence(
-        [item["input_ids"] for item in batch], batch_first=True, padding_value=0
+def collate_fn(batch):
+    tokenized_smiles = pad_sequence(
+        [item["tokenized_smiles"] for item in batch], batch_first=True, padding_value=0
     )
     attention_mask = pad_sequence(
         [item["attention_mask"] for item in batch], batch_first=True, padding_value=0
     )
-    mz = torch.stack([item["mz"] for item in batch])
-    intensity = torch.stack([item["intensity"] for item in batch])
-    index = torch.stack([item["index"] for item in batch])
-    create_next_token = torch.stack([item["create_next_token"] for item in batch])
+
+    mz = torch.tensor([item["mz"] for item in batch], dtype=torch.float).unsqueeze(1)
+    intensity = torch.tensor(
+        [item["intensity"] for item in batch], dtype=torch.float
+    ).unsqueeze(1)
+    index = torch.tensor([item["index"] for item in batch], dtype=torch.long).unsqueeze(
+        1
+    )
+    collision_energy = torch.tensor(
+        [item["collision_energy"] for item in batch], dtype=torch.long
+    ).unsqueeze(1)
+    instrument_type = torch.tensor(
+        [item["instrument_type"] for item in batch], dtype=torch.long
+    ).unsqueeze(1)
+    stop_token = torch.tensor(
+        [item["stop_token"] for item in batch], dtype=torch.float
+    ).unsqueeze(1)
 
     return {
-        "input_ids": input_ids,
+        "tokenized_smiles": tokenized_smiles,
         "attention_mask": attention_mask,
+        "index": index,
+        "collision_energy": collision_energy,
+        "instrument_type": instrument_type,
         "mz": mz,
         "intensity": intensity,
-        "index": index,
-        "create_next_token": create_next_token,
+        "stop_token": stop_token,
     }
 
 
