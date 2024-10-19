ae6d851d8579c6e6dad141ae1f3166b3defc9442
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-10-19 21:37:53 +0200

    Compute jaccard index with true intensirty

--- src/loss/mol2ms_loss.py
+++ src/loss/mol2ms_loss.py
@@ -14,16 +14,14 @@ class Mol2MSLoss(nn.Module):
         self,
         pred_mz: Float[t.Tensor, "batch seq-1"],
         mz: Float[t.Tensor, "batch seq-1"],
-        pred_intensity: Float[t.Tensor, "batch seq-1"],
+        true_intensity: Float[t.Tensor, "batch seq-1"],
     ) -> Float[t.Tensor, "1"]:
         # Create masks for valid values
         pred_mask = pred_mz >= 0
         true_mask = mz >= 0
 
         pred_mz = t.where(pred_mask, pred_mz, t.zeros_like(pred_mz))
-        pred_intensity = t.where(
-            pred_mask, pred_intensity, t.zeros_like(pred_intensity)
-        )
+        true_intensity = t.where(true_mask, true_intensity, t.zeros_like(true_intensity))
         mz = t.where(true_mask, mz, t.zeros_like(mz))
 
         pred_mz = pred_mz.unsqueeze(2)  # batch pred_seq 1
@@ -37,8 +35,8 @@ class Mol2MSLoss(nn.Module):
             t.exp(-mz_diff_matrix / self.config.soft_match_threshold).max(2).values
         )  # batch pred_seq
 
-        # Weight the closest matches by predicted intensities
-        weighted_soft_match = soft_match * pred_intensity  # batch pred_seq
+        # Weight the closest matches by true intensities
+        weighted_soft_match = soft_match * true_intensity  # batch pred_seq
 
         # Calculate intensity-weighted intersection and union
         soft_intersect = t.sum(weighted_soft_match, dim=1)  # batch
