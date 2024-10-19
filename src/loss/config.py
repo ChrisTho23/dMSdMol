2d4be7fb92fd8fe37316d67dc43cb0434f4d1a02
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-10-19 14:27:08 +0200

    Increase loss weight on sign pnealty

--- src/loss/config.py
+++ src/loss/config.py
@@ -5,7 +5,7 @@ class Mol2MSLossConfig:
     mse_mz_weight: float = 1.0
     mse_intensity_weight: float = 1.0
     soft_jaccard_weight: float = 1.0
-    sign_penalty_weight: float = 1.0
+    sign_penalty_weight: float = 3.0
 
     soft_match_threshold: float = 0.01
     epsilon: float = 1e-6
