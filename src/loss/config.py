67bf9234df1a773a042ddadb3341733225059cbd
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-10-19 18:46:12 +0200

    Adapt loss weights

--- src/loss/config.py
+++ src/loss/config.py
@@ -3,10 +3,10 @@ from dataclasses import dataclass
 
 @dataclass
 class Mol2MSLossConfig:
-    mse_mz_weight: float = 1.0
-    mse_intensity_weight: float = 1.0
-    soft_jaccard_weight: float = 1.0
-    sign_penalty_weight: float = 3.0
+    mse_mz_weight: float = 0.2
+    mse_intensity_weight: float = 0.2
+    soft_jaccard_weight: float = 2.0
+    sign_penalty_weight: float = 5.0
 
-    soft_match_threshold: float = 0.01
+    soft_match_threshold: float = 10
     epsilon: float = 1e-6
