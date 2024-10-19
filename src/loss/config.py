003c9fb70d9f36518296ac75a483b92c6394d75f
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-10-19 21:36:54 +0200

    Adjust config

--- src/loss/config.py
+++ src/loss/config.py
@@ -3,10 +3,10 @@ from dataclasses import dataclass
 
 @dataclass
 class Mol2MSLossConfig:
-    mse_mz_weight: float = 0.2
-    mse_intensity_weight: float = 0.2
-    soft_jaccard_weight: float = 2.0
+    mse_mz_weight: float = 0.5
+    mse_intensity_weight: float = 0.5
+    soft_jaccard_weight: float = 1.0
     sign_penalty_weight: float = 5.0
 
-    soft_match_threshold: float = 10
+    soft_match_threshold: float = 100
     epsilon: float = 1e-6
