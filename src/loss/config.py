874dee90365d9431851db3bbb833be4b4112099c
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-10-19 11:31:58 +0200

    Extend config based on new implementation

--- src/loss/config.py
+++ src/loss/config.py
@@ -2,7 +2,12 @@ from dataclasses import dataclass
 
 @dataclass
 class Mol2MSLossConfig:
+    mse_mz_weight: float = 1.0
+    mse_intensity_weight: float = 1.0
+    soft_jaccard_weight: float = 1.0
+    sign_penalty_weight: float = 1.0
+
     soft_match_threshold: float = 0.01
-    length_penalty_weight: float = 0.0
     epsilon: float = 1e-6
+
     
